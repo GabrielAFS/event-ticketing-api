8e85bd8101cb79f56e8ce41faa4a6b8d0da7a20a
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-03-25 20:28:56 -0300

    config: Update environment variables

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -5,11 +5,11 @@ config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: process.env.host,
-  port: +process.env.port,
-  username: process.env.username,
-  password: process.env.password,
-  database: process.env.database,
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
 };
